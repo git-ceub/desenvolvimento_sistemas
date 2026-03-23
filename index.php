9fbbd03c3db949d6a2e8a107a0ed692e575a3564
Francisco Carlos Molina Duarte Junior <francisco.carlos@dinfor.uniceub>
2026-03-23 20:54:05 -0300

    integração com o banco de dados

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    include 'conexao.php';
+    $sql = " SELECT * FROM aluno ";
+    $consulta = $conexao->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <table width="100%" border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Ações</th>
+        </tr>
+    
+        <?php
+            while ($linha = $consulta->fetch(PDO::FETCH_OBJ)) {
+        ?>
+            <tr>
+                <td><?php echo $linha->id ?></td>
+                <td><?php echo $linha->nome ?></td>
+                <td><a href="excluir.php?id=">Excluir</a></td>
+            </tr>
+        <?php 
+            } 
+        ?>
+    </table>
+
+</body>
+</html>
