32b85964080474ed01a68166219033d1e7697b25
Francisco Carlos Molina Duarte Junior <francisco.carlos@dinfor.uniceub>
2026-03-30 21:16:39 -0300

    Adicionando a funcionalidade de inserção

---
+++ script.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `aula_php`;
+USE `aula_php`;
+
+CREATE TABLE `aluno` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `idade` int DEFAULT NULL,
+  PRIMARY KEY (`id`));
+
