686dafe0b294dfb15ca1371353871c5434bc4ea3
Francisco Carlos Molina Duarte Junior <francisco.carlos@dinfor.uniceub>
2026-04-13 21:27:08 -0300

    Adicionando a funcionalidade de inserção

--- script.sql
+++ script.sql
@@ -7,5 +7,16 @@ CREATE TABLE `aluno` (
   `idade` int DEFAULT NULL,
   PRIMARY KEY (`id`));
 
+CREATE TABLE usuario (
+	id INT NOT NULL AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    senha varchar(255) NOT NULL,
+    PRIMARY KEY (`id`));
+    
+INSERT INTO usuario (nome, email, senha) VALUES ('João da Silva', 'js@gmail.com', md5(123456));
+INSERT INTO usuario (nome, email, senha) VALUES ('Maria do Rosário', 'mr@gmail.com', md5(123456));
+INSERT INTO usuario (nome, email, senha) VALUES ('Lucas Duarte', 'ld@gmail.com', md5(123456));
+
 
 
